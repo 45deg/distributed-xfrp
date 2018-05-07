cbef34cb5865deeac0cda4f56d4aa755df19a240
45deg <45deg@users.noreply.github.com>
2018-05-07 18:31:36 +0900

    Added error position

--- src/main.ml
+++ src/main.ml
@@ -4,14 +4,15 @@ let filename = Sys.argv.(1)
 
 let () =
   let input = open_in filename in
-  let filebuf = from_channel input in
+  let lexbuf = from_channel input in
   try
-    let main = Parser.prog_module Lexer.read filebuf in
+    let main = Parser.prog_module Lexer.read lexbuf in
     let ti = Typing.type_module main in
     print_endline (Codegen.of_xmodule main ti)
   with
   | Lexer.Error msg ->
-      Printf.eprintf "%s" msg
+      Printf.eprintf "%s." msg;
   | Parser.Error ->
-      Printf.eprintf "Syntax error";
+      let pos = lexbuf.lex_curr_p in
+      Printf.eprintf "Syntax Error at Line %d, Char %d." pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1);
   close_in input 
